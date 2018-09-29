c976abe88e93785cc045036e065442dd18dbed29
w28ahmad <43382198+w28ahmad@users.noreply.github.com>
2018-09-29 17:44:09 -0400

    Create prime.cpp

---
+++ prime.cpp
@@ -0,0 +1,30 @@
+// Example program
+#include <iostream>
+
+bool is_prime(unsigned int n);
+int main();
+int next_prime();
+
+bool is_prime(unsigned int n) {
+	for (unsigned int k { 2 }; k < (n); ++k) {
+
+		if ((n % k) == 0) {
+			return false;
+		}
+	}
+	std::cout << n << " is a prime" << std::endl;
+
+}
+
+int next_prime() {
+	int n { 1 };
+
+	while (n < 100) {
+		is_prime(n);
+		n++;
+	}
+}
+
+int main() {
+	next_prime();
+}
